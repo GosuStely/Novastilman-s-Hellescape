6e1f874e540538dbb9847469158c8caf6457b453
GosuStely <stely.tishelov@gmail.com>
2023-11-28 11:40:43 +0100

    wall tag

--- Assets/Scripts/Map script/The working map/Teleportation.cs
+++ Assets/Scripts/Map script/The working map/Teleportation.cs
@@ -4,13 +4,14 @@ public class Teleportation : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //where the person will teleport
         Vector3 vector3 = transform.position;
         vector3.x += 80;
         if (other.CompareTag("Player")) // Check if the colliding object is the player
         {
             // Teleport the player to the destination
             other.transform.position = vector3;
-
+            //where the camera will teleport
             Vector3 mainCameraVector3 = new Vector3();
             mainCameraVector3 = Camera.main.transform.position;
             mainCameraVector3.x += 80;
