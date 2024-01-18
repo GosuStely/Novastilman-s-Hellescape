bb5d73c70806ecb523d50fbb5c2d4da93df2cfce
GosuStely <stely.tishelov@gmail.com>
2024-01-18 12:43:24 +0100

    some thing improved

--- Assets/Player/Player Scripts/Inventory.cs
+++ Assets/Player/Player Scripts/Inventory.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
-using UnityEditor.Search;
+//using UnityEditor.Search;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
